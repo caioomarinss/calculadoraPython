59e4a9097f27a248edceb04402bd580a7305f509
Caio Marins <caio.alice.marins@gmail.com>
2025-07-03 22:14:29 -0300

    Commit de organizacao

--- script.py
+++ script.py
@@ -1,36 +1,33 @@
-import tkinter as tk #importacao da biblioteca para criar as telas
+import tkinter as tk #importacao da biblioteca para criar a interface grafica
 
-telaSimples = tk.Tk() #criacao de uma tela
+janela = tk.Tk() #criacao da janela principal
 
-resultado = tk.Label(telaSimples, text="Resultado: ") #crio um texto embaixo da calculadora para dizer "resultado:" para que o resultado da conta nao fique sozinho.
-resultado.grid(row=5, column=4) #posiciono ele na linha 5 e coluna 4
+textoResultado = tk.Label(janela, text="Resultado: ") #criacao de um texto na janela para informar o resultado ao usuario
+textoResultado.grid(row=5, column=1, columnspan=3) #posiciono ele na parte inferior da janela
 
-equation = tk.StringVar() #cria uma variavel do tipo StringVar, ela é usada para armazenar textos dinamicos que podem ser atualizados na tela automaticamente
-equation.set("0") #seta o valor da variavel como 0
-display = tk.Label(telaSimples, textvariable=equation) #cria um texto na janela, que mostra o valor da variavel "equation" que recebe o valor da variavel "resultado" que é o resultado das operacaoes
-display.grid(row=5, column=5) #posiciono o texto na linha 4 e na coluna 5.
+resultadoOperacao = tk.StringVar() #criacao de uma variavel do tipo StringVar, ela é usada para armazenar textos dinamicos que podem ser atualizados na tela automaticamente conforme o valor
 
-texto = tk.Label(telaSimples, text="Calculadora!") #adionando um nome a tela
-texto.grid(row=0, column=0, columnspan=4) #mostra esse nome na tela na primeira linha e na primeira coluna, o columnspan no valor 4 define que o texto vai ficar centralizado entre 4 colunas
+textoResultadoOperacao = tk.Label(janela, textvariable=resultadoOperacao) #criacao de um texto na janela para mostrar o valor da variavel "resultadoOperacao" que recebe o valor da variavel "resultado"
+textoResultadoOperacao.grid(row=5, column=4) #posiciono o texto na linha 5 e na coluna 4, ao lado do texto "Resultado:" criado na linha 5 deste codigo
 
-vez = 1 #crio uma variavel para definir a vez das variaveis n1 e n2 para saber quem vai receber o valor quando o botao for clicado
+vez = 1 #criacao de uma variavel para definir a vez das variaveis n1 e n2, para definir qual vai receber o valor quando o botao for clicado
 
-def valorN(valor): #define a funcao valorN que quando chamada recebe o parametro "valor", "valor" recebe o numero que for passado da seguinte forma: valorN(0), quer dizer que "valor" é 0.
-    global n1, n2, vez #coloco essas variaveis no escopo global para poderem ser utilizadas em todo o codigo
+def valorNumero(vN): #define a funcao valorNumero, que quando chamada recebe o parametro "vN" que recebe o numero que for passado da seguinte forma: valorNumero(0), significa que "vN" recebe 0
+    global n1, n2, vez #variaveis no escopo global, podem ser utilizadas em todo o codigo
     if vez == 1:
-        n1 = valor  #se tiver na vez 1, o n1 que vai receber o valor do botao
-        vez = 2 #troco o vez para que o segundo clique no botao seja o valor de n2
-    elif vez == 2: #se a vez for 2, o que quer dizer que n1 ja recebeu um valor, entao agora o valor do botao clicado vai para n2
-            n2 = valor #n2 recebe o valor do botao clicado
-            vez = 1 #volto o valor da vez para 1, se o usuario cliar novamente no botao agora ira redefinir o valor de n1 e se clicar mais uma vez, ira redefinir o valor de n2 e etc
+        n1 = vN  #se vez é igual a 1, n1 recebera o valor do botao clicado
+        vez = 2 #a vez se alterna para que n2 receba o valor do proximo botao clicado
+    elif vez == 2: #se vez é igual a 2, significa que n1 ja recebeu um valor, entao a vez alterna para que n2 receba o proximo valor
+            n2 = vN #n2 recebe o valor do botao clicado
+            vez = 1 #o valor da vez volta para 1, o valor de n1 sera substituido caso o usuario clique novamente no botao
 
-def valorOperacao(op): #define a funcao valorOperacao que quando chamada recebe o parametro "op" que recebe o valor da seguinte forma: valorOperacao('+'), quer dizer que "op" é '+'
+def valorOperacao(vO): #define a funcao valorOperacao, que quando chamada recebe o parametro "vO" que recebe a operacao escolhida da seguinte forma: valorOperacao('+'), significa que "vO" recebe '+'
     global operacao
-    operacao = op #a variavel operacao recebe "op" que é a operacao escolhida
+    operacao = vO #a variavel "operacao" recebe "vO" que é igual a operacao escolhida
 
-def igual(): #define a funcao igual que quando chamada verifica qual o valor da variavel "operacao" e dependendo desse valor faz a conta que o usuario pediu
+def calcular(): #define a funcao calcular, que quando chamada verifica qual o valor da variavel "operacao" e dependendo desse valor, realiza uma determinada operacao escolhida pelo usuario
     global n1, n2, operacao
-    if operacao == '+': #se o usuario escolheu o botao "+" ele vai receber uma conta de soma, e etc
+    if operacao == '+': #se o usuario apertou o botao "+" ele vai receber uma operacao de soma, e etc
         resultado = n1 + n2
     elif operacao == '-':
         resultado = n1 - n2
@@ -38,24 +35,23 @@ def igual(): #define a funcao igual que quando chamada verifica qual o valor da 
         resultado = n1 * n2
     elif operacao == '/':
         resultado = n1 / n2
-    equation.set(str(resultado)) #a variavel "equation" recebe o valor de "resultado", para que apareca o valor do resultado da operacao na janela
+    resultadoOperacao.set(str(resultado)) #a variavel "resultadoOperacao" recebe o valor de "resultado", agora o resultado da operacao ira aparecer na parte inferior da janela gracas a linha 8 do codigo
 
-tk.Button(telaSimples, text="0", command=lambda: valorN(0)).grid(row=1, column=0) #crio um botão com texto "0" que ao ser clicado vai dar o valor para o parametro "valor", n1 ou n2 irá receber o conteudo de "valor".
-tk.Button(telaSimples, text="1", command=lambda: valorN(1)).grid(row=1, column=1)
-tk.Button(telaSimples, text="2", command=lambda: valorN(2)).grid(row=1, column=2)
-tk.Button(telaSimples, text="+", command=lambda: valorOperacao('+')).grid(row=1, column=3) #cria um botão com texto "+" que ao ser clicado vai dar o valor para o parametro "op", operacao ira receber esse valor
-tk.Button(telaSimples, text="3", command=lambda: valorN(3)).grid(row=2, column=0)
-tk.Button(telaSimples, text="4", command=lambda: valorN(4)).grid(row=2, column=1)
-tk.Button(telaSimples, text="5", command=lambda: valorN(5)).grid(row=2, column=2)
-tk.Button(telaSimples, text="-", command=lambda: valorOperacao('-')).grid(row=2, column=3)
-tk.Button(telaSimples, text="6", command=lambda: valorN(6)).grid(row=3, column=0)
-tk.Button(telaSimples, text="7", command=lambda: valorN(7)).grid(row=3, column=1)
-tk.Button(telaSimples, text="8", command=lambda: valorN(8)).grid(row=3, column=2)
-tk.Button(telaSimples, text="x", command=lambda: valorOperacao('*')).grid(row=3, column=3)
-tk.Button(telaSimples, text="9", command=lambda: valorN(9)).grid(row=4, column=0)
-tk.Button(telaSimples, text="=", command=igual).grid(row=4, column=1)
-tk.Button(telaSimples, text="/", command=lambda: valorOperacao('/')).grid(row=4, column=3)
+tk.Button(janela, text="0", command=lambda: valorNumero(0)).grid(row=4, column=1) #criacao dos botãoes com texto dos numeros que ao serem clicados vao dar o valor para o parametros "vN",
+tk.Button(janela, text="1", command=lambda: valorNumero(1)).grid(row=3, column=0) #n1 e n2 irao receber o conteudo do parametro "vN"
+tk.Button(janela, text="2", command=lambda: valorNumero(2)).grid(row=3, column=1)
+tk.Button(janela, text="3", command=lambda: valorNumero(3)).grid(row=3, column=2)
+tk.Button(janela, text="4", command=lambda: valorNumero(4)).grid(row=2, column=0)
+tk.Button(janela, text="5", command=lambda: valorNumero(5)).grid(row=2, column=1)
+tk.Button(janela, text="6", command=lambda: valorNumero(6)).grid(row=2, column=2)
+tk.Button(janela, text="7", command=lambda: valorNumero(7)).grid(row=1, column=0)
+tk.Button(janela, text="8", command=lambda: valorNumero(8)).grid(row=1, column=1)
+tk.Button(janela, text="9", command=lambda: valorNumero(9)).grid(row=1, column=2)
 
-telaSimples.mainloop() #Mostra a tela ate ela ser fechada
+tk.Button(janela, text="+", command=lambda: valorOperacao('+')).grid(row=1, column=3) #criacao dos botoes com texto das operacoes que ao serem clicados vao dar o valor para o parametro "vO",
+tk.Button(janela, text="-", command=lambda: valorOperacao('-')).grid(row=2, column=3) #a variavel "operacao" ira receber o conteudo do parametro "vO"
+tk.Button(janela, text="x", command=lambda: valorOperacao('*')).grid(row=3, column=3)
+tk.Button(janela, text="/", command=lambda: valorOperacao('/')).grid(row=4, column=3)
+tk.Button(janela, text="=", command=calcular).grid(row=4, column=2)
 
-
+janela.mainloop() #Mostra a janela ate ela ser fechada
